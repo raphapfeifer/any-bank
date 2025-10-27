408dfce1817bdbd2f1c67bb330af72b183e00c0d
unknown <raphael.alcateia>
2025-10-27 14:41:58 -0300

    account component

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,12 +1,14 @@
 import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { AccountComponent } from "./account/account.component";
 
 @Component({
   selector: 'app-banner',
-  imports: [DatePipe],
+  imports: [WelcomeComponent, AccountComponent],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
-    currentDate = new Date();
+    
 }
