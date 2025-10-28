3e9f6ff1ea80d06f01ee83bd2b46807915a0d19e
unknown <raphael.alcateia>
2025-10-28 14:17:22 -0300

    Internationalization and titlecase

--- src/app/banner/account/account.component.ts
+++ src/app/banner/account/account.component.ts
@@ -1,8 +1,9 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-account',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
