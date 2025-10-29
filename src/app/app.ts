28273374350d3746b274e5fdf7c2bebbf1272a53
unknown <raphael.alcateia>
2025-10-29 14:35:02 -0300

    New transaction component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
+import { NewFormTransctionComponent } from "./new-form-transction/new-form-transction.component";
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent],
+  imports: [BannerComponent, NewFormTransctionComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/new-form-transction/new-form-transction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-form-transction',
+  imports: [],
+  templateUrl: './new-form-transction.component.html',
+  styleUrl: './new-form-transction.component.css'
+})
+export class NewFormTransctionComponent {
+
+}
